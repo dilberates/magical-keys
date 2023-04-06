1374047fdbb1505924d41e0a80cf5601935ac0f1
iremsmr <iremsamur129@gmail.com>
2023-04-06 23:59:40 +0300

    admin layout page is added

--- music-project/src/components/admin/admin_main_page.js
+++ music-project/src/components/admin/admin_main_page.js
@@ -1,8 +1,16 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import AdminMenuLayout from './admin_menu_layout';
+import AdminSidebarLayout from './admin_sidebar_layout';
 export default function AdminMainPage()  {
  
     return (
-        <div>admin_main_page</div>
+      <div>admin main page
+        <AdminMenuLayout></AdminMenuLayout>
+        <AdminSidebarLayout></AdminSidebarLayout>
+        <main id="main" class="main">
+          
+        </main>
+      </div>
+  
       )
 }

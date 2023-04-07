a39f81d80cc2672f04aa77bd45a9cd0350f492dd
iremsmr <iremsamur129@gmail.com>
2023-04-07 21:07:13 +0300

    Admin Level Adding Page is added

---
+++ MusicProjectBackendCodes/models/Level.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const LevelSchema = new mongoose.Schema(
+    {
+    level_title:String,
+    level_description:String,
+    level_status:Boolean
+    
+},{
+    collection:"Level",
+}
+);
+mongoose.model("Level",LevelSchema);

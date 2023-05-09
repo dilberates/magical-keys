db47d065cb113f509147392e21abb27da8739095
iremsmr <iremsamur129@gmail.com>
2023-05-09 23:54:35 +0300

    level_priority and content_priority fields are added

--- MusicProjectBackendCodes/models/Level.js
+++ MusicProjectBackendCodes/models/Level.js
@@ -4,7 +4,8 @@ const LevelSchema = new mongoose.Schema(
     level_title:String,
     level_description:String,
     level_status:Boolean,
-    level_image:String
+    level_image:String,
+    level_priority:Number
     
 },{
     collection:"Level",

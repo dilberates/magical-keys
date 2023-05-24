38ca02cefb18d7a76f3ed66d256f670a2361b070
Dilber KILIÇ <48033000+dilberkilic@users.noreply.github.com>
2023-05-24 18:22:20 +0300

    Update Admin panel
    Update Admin panel

--- music-project/src/components/admin/admin_sidebar_layout.js
+++ music-project/src/components/admin/admin_sidebar_layout.js
@@ -15,12 +15,7 @@ export default function AdminSidebarLayout() {
 
     <ul className="sidebar-nav" id="sidebar-nav">
 
-      <li className="nav-item">
-        <a className="nav-link " href="index.html">
-          <i className="bi bi-grid"></i>
-          <span>Dashboard</span>
-        </a>
-      </li>
+     
 
       <li className="nav-item">
         <a className="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="http://localhost:3000/getLevelsPage">
